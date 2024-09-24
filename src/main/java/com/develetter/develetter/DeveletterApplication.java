3d94e2c174606716b997d3d56d7a6a8b5104eb54
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-24 16:40:53 +0900

    Initial commit

---
+++ src/main/java/com/develetter/develetter/DeveletterApplication.java
@@ -0,0 +1,13 @@
+package com.develetter.develetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveletterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develetter/develetter/DeveletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develetter.develetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
