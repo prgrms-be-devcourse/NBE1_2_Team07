2ecb224786106dc10677f962d5adfffb78f37cdc
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-25 20:01:56 +0900

    Feat: dto parsing helper 예외처리 추가

--- src/main/java/com/develetter/develetter/global/util/DtoUtil.java
+++ src/main/java/com/develetter/develetter/global/util/DtoUtil.java
@@ -1,25 +1,32 @@
 package com.develetter.develetter.global.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.lang.reflect.RecordComponent;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class DtoUtil {
 
     public static Map<String, Object> toMap(Object record) {
         Map<String, Object> result = new HashMap<>();
 
-        // record 만 사용 가능
+        // record만 사용 가능
         if (record instanceof Record) {
             RecordComponent[] components = record.getClass().getRecordComponents();
             for (RecordComponent component : components) {
                 try {
                     result.put(component.getName(), component.getAccessor().invoke(record));
                 } catch (Exception e) {
-                    throw new RuntimeException(e);
+                    log.error("Error occurred while parsing record component: " + component.getName(), e);
+                    throw new RuntimeException("Error occurred while parsing record component: " + component.getName(), e);
                 }
             }
+        } else {
+            log.warn("The provided object is not a Record: " + record.getClass().getName());
         }
+
         return result;
     }
 }
