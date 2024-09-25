2844eb30cb71fb2bb0d1c9b603a63b7a4fb882d3
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-25 22:08:51 +0900

    Feat: 테스트를 위한 사람인 채용공고 API 호출 logic 변경

--- src/main/java/com/develetter/develetter/jobposting/service/JobPostingServiceImpl.java
+++ src/main/java/com/develetter/develetter/jobposting/service/JobPostingServiceImpl.java
@@ -3,6 +3,8 @@ package com.develetter.develetter.jobposting.service;
 import com.develetter.develetter.global.util.DtoUtil;
 import com.develetter.develetter.jobposting.dto.JobSearchReqDto;
 import com.develetter.develetter.jobposting.dto.JobSearchResDto;
+import com.develetter.develetter.jobposting.entity.JobPosting;
+import com.develetter.develetter.jobposting.repository.JobPostingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +12,9 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,11 +31,11 @@ public class JobPostingServiceImpl implements JobPostingService {
     private String baseUrl;
 
     private final WebClient webClient;
+    private final JobPostingRepository jobPostingRepository;
 
 
     public JobSearchResDto searchJobs(JobSearchReqDto jobSearchReqDto) {
-        Map<String, Object> params = DtoUtil.toMap(jobSearchReqDto);
-
+//        Map<String, Object> params = DtoUtil.toMap(jobSearchReqDto);
 
 //        return webClient.get()
 //                .uri(uriBuilder -> {
@@ -49,14 +52,31 @@ public class JobPostingServiceImpl implements JobPostingService {
 //                .bodyToMono(JobSearchResDto.class)
 //                .block(); // 동기 처리
 
-        String testURL = "http://localhost:8080/test";
+        String testURL = "http://localhost:8080/jobposting/test";
+        try {
+            JobSearchResDto jobsDto = webClient.get()
+                    .uri(testURL)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono(JobSearchResDto.class)
+                    .block();// 동기 처리
 
-        return webClient.get()
-                .uri(testURL)
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(JobSearchResDto.class)
-                .block(); // 동기 처리
+            JobSearchResDto.Jobs jobs = jobsDto.jobs();
+
+            // 여기서 jobs.job()을 호출하여 List<Job>에 접근
+            List<JobPosting> jobPostings = jobs.job().stream()
+                    .map(JobSearchResDto.Job::toEntity)
+                    .collect(Collectors.toList());
+
+            jobPostingRepository.saveAll(jobPostings);  // 여러 엔티티를 한 번에 저장
+            log.info("success [searchJobs]");
+
+            return jobsDto;
+
+        } catch (Exception e) {
+            log.error("Error occurred during [searchJobs] ", e);
+            throw e;
+        }
     }
 }
 
