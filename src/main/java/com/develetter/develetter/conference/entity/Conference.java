04c1b5f9229e12a63432dff4ab440025f23ef5b6
rudgns328 <rudgns328@gmail.com>
2024-09-26 17:13:59 +0900

    Feat: Conference Entity, Repository 작성

---
+++ src/main/java/com/develetter/develetter/conference/entity/Conference.java
@@ -0,0 +1,49 @@
+package com.develetter.develetter.conference.entity;
+
+import com.develetter.develetter.conference.dto.ConferenceRegisterDto;
+import com.develetter.develetter.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "conference")
+public class Conference extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "host", nullable = false)
+    private String host;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    public void updateConference(ConferenceRegisterDto dto) {
+        this.name = dto.name();
+        this.host = dto.host();
+        this.startDate = dto.startDate();
+        this.endDate = dto.endDate();
+        this.url = dto.url();
+    }
+}
