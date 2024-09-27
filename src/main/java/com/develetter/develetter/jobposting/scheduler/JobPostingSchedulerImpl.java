473f1a78ae214b3a133f81f2d432a45665c2f2aa
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-27 17:06:53 +0900

    refactor: delete example code

--- src/main/java/com/develetter/develetter/jobposting/scheduler/JobPostingSchedulerImpl.java
+++ src/main/java/com/develetter/develetter/jobposting/scheduler/JobPostingSchedulerImpl.java
@@ -1,15 +1,11 @@
 package com.develetter.develetter.jobposting.scheduler;
 
-import com.develetter.develetter.jobposting.dto.JobSearchReqDto;
-import com.develetter.develetter.jobposting.repository.JobPostingRepository;
 import com.develetter.develetter.jobposting.service.JobPostingService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -20,27 +16,13 @@ public class JobPostingSchedulerImpl implements JobPostingScheduler {
 
     private final JobPostingService jobPostingService;
 
-//    private final
-
     @Override
 //    @Scheduled(cron = "0 0 0 * * *") // 매일 00:00시 정각에 실행
-    @Scheduled(fixedRate = 5000) // 5초마다 호출 테스트
+//    @Scheduled(fixedRate = 5000) // 5초마다 호출 테스트
     @Transactional
     public void fetchJobPostings() {
 
-        jobPostingService.searchJobs(null); // param test -> null
+//        jobPostingService.searchJobs(); // param test -> null
     }
-//
-//    @Scheduled(cron = "0 0 14 * * ?")
-//    @Transactional
-//    public void updateOrdersToShippingStarted() {
-//        LocalDateTime now = LocalDateTime.now();
-//        LocalDateTime today2pm = now.withHour(14).withMinute(0).withSecond(0).withNano(0);
-//        LocalDateTime yesterday2pm = today2pm.minusDays(1);
-//
-//        int updatedCount = orderRepository.updateOrderStatusInDateRange(
-//                OrderStatus.SHIPPING_STARTED, OrderStatus.ORDER_COMPLETED, yesterday2pm, today2pm, now
-//        );
-//
-//    }
+
 }
