579397e93ba4e6f078f56f906bc5f67c290e7e3b
rudgns328 <rudgns328@gmail.com>
2024-09-27 12:06:21 +0900

    Feat: Conference service 시작일, 종료일 기준으로 해당하는 컨퍼런스 반환하는 메서드 추가

--- src/main/java/com/develetter/develetter/conference/service/ConferenceService.java
+++ src/main/java/com/develetter/develetter/conference/service/ConferenceService.java
@@ -4,10 +4,13 @@ import com.develetter.develetter.conference.dto.ConferenceRegisterDto;
 import com.develetter.develetter.conference.dto.ConferenceResDto;
 import jakarta.validation.Valid;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ConferenceService {
     List<ConferenceResDto> getAllConference();
+
+    List<ConferenceResDto> getAllConferenceWithDateRange(LocalDate start, LocalDate end);
 
     void createConference(ConferenceRegisterDto conferenceRegisterDto);
 
