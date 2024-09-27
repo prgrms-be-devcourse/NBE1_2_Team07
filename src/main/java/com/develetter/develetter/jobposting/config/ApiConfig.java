707ee57a3e64f677d53962ac8ca44b084a581b0a
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-27 17:14:03 +0900

    Feat: 인코딩 모드 설정

--- src/main/java/com/develetter/develetter/jobposting/config/ApiConfig.java
+++ src/main/java/com/develetter/develetter/jobposting/config/ApiConfig.java
@@ -8,17 +8,17 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 @Configuration
 public class ApiConfig {
 
-    @Bean
-    public DefaultUriBuilderFactory builderFactory() { // encoding 모드 지정해줘서 service key 값 달라지는 것 방지
-        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
-        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
-        return factory;
-    }
+//    @Bean
+//    public DefaultUriBuilderFactory builderFactory() { // encoding 모드 지정해줘서 accessKey 값 달라지는 것 방지
+//        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
+//        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+//        return factory;
+//    }
 
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .uriBuilderFactory(builderFactory())
+//                .uriBuilderFactory(builderFactory())
                 .build();
     }
 }
