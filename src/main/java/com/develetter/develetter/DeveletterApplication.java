0251228742f67b7b491562e007e1b05aa72dfeb1
최철진 <cil6879@gmail.com>
2024-09-27 16:08:47 +0900

    어플리케이션 파일 커밋 잘못 올린것 수정

--- src/main/java/com/develetter/develetter/DeveletterApplication.java
+++ src/main/java/com/develetter/develetter/DeveletterApplication.java
@@ -8,9 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing // 등록시각, 수정시각을 위한 전체 auditing 활성화를 위한 에노테이션
 @EnableScheduling // 스케줄링 활성화
 @SpringBootApplication
-@EnableScheduling
 public class DeveletterApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DeveletterApplication.class, args);
 	}
