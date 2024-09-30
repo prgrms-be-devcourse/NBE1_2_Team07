2b4410a206a91fc96c9009fb61c020cbdd6405aa
rudgns328 <rudgns328@gmail.com>
2024-09-30 11:39:32 +0900

    Fix: JobPostingController scheduler 메서드 호출하도록 변경

--- src/main/java/com/develetter/develetter/jobposting/controller/JobPostingController.java
+++ src/main/java/com/develetter/develetter/jobposting/controller/JobPostingController.java
@@ -2,21 +2,28 @@ package com.develetter.develetter.jobposting.controller;
 
 import com.develetter.develetter.global.dto.ApiResponseDto;
 import com.develetter.develetter.jobposting.dto.JobSearchResDto;
+import com.develetter.develetter.jobposting.scheduler.JobPostingSchedulerImpl;
 import com.develetter.develetter.jobposting.service.JobPostingService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/jobposting")
 @RequiredArgsConstructor
+@Slf4j
 public class JobPostingController {
 
     private final JobPostingService jobPostingService;
+    private final JobPostingSchedulerImpl jobPostingSchedulerImpl;
 
     @GetMapping("/testCall")
     public ApiResponseDto<JobSearchResDto> callJobSearchApi() {
         // DTO를 Service로 전달
-        JobSearchResDto jobPosting = jobPostingService.searchJobs();
-        return new ApiResponseDto<>(200, "성공", jobPosting);
+        jobPostingSchedulerImpl.fetchJobPostings();
+
+        log.info("success");
+
+        return new ApiResponseDto<>(200, "성공");
     }
 }
