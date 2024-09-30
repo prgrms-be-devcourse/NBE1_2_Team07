55c088dfc876808b248f87167b96576e17801aed
rudgns328 <rudgns328@gmail.com>
2024-09-30 12:11:38 +0900

    Fix: ApiConfig 사용하지 않는 import문 삭제

--- src/main/java/com/develetter/develetter/jobposting/config/ApiConfig.java
+++ src/main/java/com/develetter/develetter/jobposting/config/ApiConfig.java
@@ -3,12 +3,11 @@ package com.develetter.develetter.jobposting.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class ApiConfig {
 
-//    @Bean
+    //    @Bean
 //    public DefaultUriBuilderFactory builderFactory() { // encoding 모드 지정해줘서 accessKey 값 달라지는 것 방지
 //        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
 //        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
