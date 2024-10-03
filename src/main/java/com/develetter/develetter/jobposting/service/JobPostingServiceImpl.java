346500301496457a1d37c4983e55f24981f0a729
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-03 17:18:50 +0900

    Feat: JobPosting -> userFilter 적용
    test Controller 포함

--- src/main/java/com/develetter/develetter/jobposting/service/JobPostingServiceImpl.java
+++ src/main/java/com/develetter/develetter/jobposting/service/JobPostingServiceImpl.java
@@ -1,12 +1,18 @@
 package com.develetter.develetter.jobposting.service;
 
 import com.develetter.develetter.global.util.DtoUtil;
+import com.develetter.develetter.jobposting.UserFilter;
+import com.develetter.develetter.jobposting.UserFilterRepository;
 import com.develetter.develetter.jobposting.converter.Converter;
 import com.develetter.develetter.jobposting.dto.JobSearchParams;
 import com.develetter.develetter.jobposting.dto.JobSearchResDto;
+import com.develetter.develetter.jobposting.entity.FilteredJobPosting;
 import com.develetter.develetter.jobposting.entity.JobPosting;
 import com.develetter.develetter.jobposting.exception.JobSearchException;
+import com.develetter.develetter.jobposting.repository.FilteredJobPostingRepository;
 import com.develetter.develetter.jobposting.repository.JobPostingRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +20,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -32,6 +39,11 @@ public class JobPostingServiceImpl implements JobPostingService {
 
     private final WebClient webClient;
     private final JobPostingRepository jobPostingRepository;
+
+    // TODO 유저필터 레포지토리를 주입받지않고, 서비스를 주입받는게 좋지 않을까?
+    private final UserFilterRepository userFilterRepository;
+
+    private final FilteredJobPostingRepository filteredJobPostingRepository;
 
     @Override
     public JobSearchResDto searchJobs(int startPage) {
@@ -68,7 +80,7 @@ public class JobPostingServiceImpl implements JobPostingService {
                     .collect(Collectors.toList());
 
             jobPostingRepository.saveAll(jobPostings);  // 여러 엔티티를 한 번에 저장
-            log.info("success [searchJobs]");
+            log.info("success [searchJobs] page: {}", startPage);
 
             return jobSearchResDto;
 
@@ -76,5 +88,40 @@ public class JobPostingServiceImpl implements JobPostingService {
             log.error("Error occurred during [searchJobs] ", e);
             throw e;
         }
+    }
+
+    @Override
+    @Transactional
+    public void filterJobPostingByKeywords(Long userId) {
+        UserFilter userFilter = userFilterRepository.findByUserId(userId).orElseThrow(EntityNotFoundException::new);
+        String jobpostingKeywords = userFilter.getJobpostingKeywords();
+
+        List<String> keywords = Arrays.stream(jobpostingKeywords.split(","))
+                .map(String::trim)
+                .toList();
+
+        List<Long> jobPostingIds = jobPostingRepository.findAll().stream()
+                .filter(jobPosting -> keywords.stream().anyMatch(keyword ->
+                        jobPosting.getJobName().toLowerCase().contains(keyword.toLowerCase())))
+                .map(JobPosting::getId)
+                .collect(Collectors.toList());
+
+        // TODO: compare - Java VS queryDSL
+
+        saveFilteredJobPostings(userId, jobPostingIds);
+    }
+
+    @Override
+    public void saveFilteredJobPostings(Long userId, List<Long> jobPostings) {
+        String jobPostingList = jobPostings.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+
+        FilteredJobPosting filteredJobPosting = FilteredJobPosting.builder()
+                .userId(userId)
+                .jobPostings(jobPostingList)
+                .build();
+
+        filteredJobPostingRepository.save(filteredJobPosting);
     }
 }
