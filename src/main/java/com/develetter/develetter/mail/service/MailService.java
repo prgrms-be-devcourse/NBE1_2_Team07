30ed809f3591dce94074ced5d345449f3cc4eed9
ayoung-dev <dkdudab@hanmail.net>
2024-10-04 10:26:17 +0900

    Feat: MailService 인터페이스 생성

--- src/main/java/com/develetter/develetter/mail/service/MailService.java
+++ src/main/java/com/develetter/develetter/mail/service/MailService.java
@@ -1,56 +1,13 @@
 package com.develetter.develetter.mail.service;
+import com.develetter.develetter.mail.dto.MailResDto;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-@Transactional(readOnly = true)
-public class MailService {
-    private final AsyncMailService asyncMailService;
-    private final CalendarService calendarService;
+public interface MailService {
 
-//    @Transactional
-//    // 매분 5초마다
-//    @Scheduled(cron = "5 * * * * *")
-//    // 월요일 오전 8시마다
-//    //@Scheduled(cron = "0 0 8 * * MON")
-//    public void scheduleSaveEmail() {
-//        try {
-//        } catch (Exception e) {
-//
-//        }
-//    }
+    List<MailResDto> getAllMails();
 
-    @Transactional
-    // 매분 10초마다
-    @Scheduled(cron = "10 * * * * *")
-    // 월요일 오전 9시마다
-    //@Scheduled(cron = "0 0 9 * * MON")
-    public void scheduleEmailCron(){
-        try {
-            //conference Calendar 생성
-            String conferenceHtml = calendarService.createConferenceCalendar();
+    void createMails();
 
-            String email1 = "dkdudab@hanmail.net";
-            String email2 = "dkdudab@naver.com";
-            List<String> emails = new ArrayList<>();
-            emails.add(email1);
-            emails.add(email2);
-            for (String email : emails) {
-            /////////////////////////////////////////
-                asyncMailService.sendMail(email, conferenceHtml);
-            }
-
-        } catch (Exception e) {
-            log.error("Scheduled Mail Sent Error");
-        }
-    }
-
+    void updateMailSendingCheck(Long id);
 }
