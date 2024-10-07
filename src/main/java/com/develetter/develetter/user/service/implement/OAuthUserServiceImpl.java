bbe821f03dfed55611fc60b6f452a738edc479e9
gunwoo08 <rick9808@gmail.com>
2024-10-07 14:23:08 +0900

    fix: Oauth login password, email encrypt

--- src/main/java/com/develetter/develetter/user/service/implement/OAuthUserServiceImpl.java
+++ src/main/java/com/develetter/develetter/user/service/implement/OAuthUserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.develetter.develetter.user.service.implement;
 
 import com.develetter.develetter.user.global.entity.CustomOAuthUser;
-import com.develetter.develetter.user.repository.UserRepository;
 import com.develetter.develetter.user.global.entity.UserEntity;
+import com.develetter.develetter.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -21,6 +23,7 @@ import java.util.Map;
 public class OAuthUserServiceImpl extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
+    private PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
 
     /**
      * OAuth2 인증 후 사용자 정보를 처리하는 메서드.
@@ -31,17 +34,19 @@ public class OAuthUserServiceImpl extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
 
+
         // 기본 OAuth2UserService를 사용해 사용자 정보 로드
         OAuth2User oAuth2User = super.loadUser(request);
 
         // 어떤 OAuth 클라이언트를 사용했는지 확인 (예: 카카오, 네이버)
         String oauthClientName = request.getClientRegistration().getClientName();
+        String oauthClientId = request.getClientRegistration().getClientId();
 
         // 사용자 엔티티 및 ID 변수 선언
         UserEntity userEntity = null;
         String accountId = null;
-        String password = "Pa33w0rD";  // 기본 비밀번호
-        String email = "email@email.com";  // 기본 이메일 (OAuth 제공자에서 이메일 제공하지 않을 경우 대비)
+        String password = passwordEncoder.encode(oauthClientId);  // 각 ClientId를 비밀번호로 Encrypt
+        String email = oauthClientId+"@email.com";  // 기본 이메일을
 
         // 카카오 OAuth 처리
         if (oauthClientName.equals("kakao")) {
@@ -56,6 +61,7 @@ public class OAuthUserServiceImpl extends DefaultOAuth2UserService {
             email = responseMap.get("email");  // 네이버 사용자의 이메일 추출
             userEntity = new UserEntity(null, accountId, password, email, "naver", "ROLE_USER");  // 사용자 엔티티 생성
         }
+
         // 사용자 정보 저장
         userRepository.save(userEntity);
 
