69ba0d2664f110562456b741ef085be3f9a5020a
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-08 16:36:22 +0900

    Refactor: 의존성 주입 정리

--- src/main/java/com/develetter/develetter/jobposting/controller/JobPostingController.java
+++ src/main/java/com/develetter/develetter/jobposting/controller/JobPostingController.java
@@ -3,7 +3,6 @@ package com.develetter.develetter.jobposting.controller;
 import com.develetter.develetter.global.dto.ApiResponseDto;
 import com.develetter.develetter.jobposting.dto.JobSearchResDto;
 import com.develetter.develetter.jobposting.scheduler.JobPostingSchedulerImpl;
-import com.develetter.develetter.jobposting.service.JobPostingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.*;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class JobPostingController {
 
-    private final JobPostingService jobPostingService;
     private final JobPostingSchedulerImpl jobPostingSchedulerImpl;
     private final JobLauncher jobLauncher;
     private final Job filterJobPostingsJob;
