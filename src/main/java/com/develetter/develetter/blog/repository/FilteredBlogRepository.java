3d85c0bbeb55d39cea92c36ed2fc6504e1cf9a94
최철진 <cil6879@gmail.com>
2024-10-08 22:43:42 +0900

    Feat : 기존 메소드들 일부 수정하여 사용자가 받을 수 있는 블로그 글이 없는 상황에 대한 로직 구현, userId 받아 blog 데이터 반환하는 메소드 추가

--- src/main/java/com/develetter/develetter/blog/repository/FilteredBlogRepository.java
+++ src/main/java/com/develetter/develetter/blog/repository/FilteredBlogRepository.java
@@ -4,7 +4,12 @@ import com.develetter.develetter.blog.entity.FilteredBlog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FilteredBlogRepository extends JpaRepository<FilteredBlog, Long> {
     boolean existsByUserIdAndBlog(Long userId, Long blog); // UserFilter 기반 블로그 중복 확인
+
+    // 특정 userId에 해당하는 가장 최근의 FilteredBlog를 반환
+    Optional<FilteredBlog> findTopByUserIdOrderByCreatedAtDesc(Long userId);
 }
