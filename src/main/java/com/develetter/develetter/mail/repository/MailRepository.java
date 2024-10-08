8d8c2c1189a10089892f37ac94194798ff63a04b
ayoung-dev <dkdudab@hanmail.net>
2024-10-08 16:00:32 +0900

    Feat: userId를 기준으로 filtered_job_posting과 filtered_blog 데이터 가져오는 쿼리 추가

--- src/main/java/com/develetter/develetter/mail/repository/MailRepository.java
+++ src/main/java/com/develetter/develetter/mail/repository/MailRepository.java
@@ -11,10 +11,10 @@ import java.util.Optional;
 public interface MailRepository extends JpaRepository<Mail, Long> {
 
     // userId를 기준으로 filtered_job_posting과 filtered_blog 테이블을 JOIN 하여 필요한 데이터 가져오는 쿼리
-//    @Query("SELECT u.id, jp.id, bl.id FROM User u " +
-//            "LEFT JOIN FilteredJobPosting jp ON u.id = jp.userId " +
-//            "LEFT JOIN FilteredBlogPosting bl ON u.id = bl.userId")
-//    List<Object[]> findAllUserWithFilteredData();
+    @Query("SELECT u.id, jp.id, bl.id FROM user u " +
+            "LEFT JOIN FilteredJobPosting jp ON u.id = jp.userId " +
+            "LEFT JOIN FilteredBlog bl ON u.id = bl.userId")
+    List<Object[]> findAllUserWithFilteredData();
 
     @Query("SELECT m FROM Mail m WHERE m.deleted = false AND m.sendingCheck = false")
     Optional<List<Mail>> findBySendingCheckIsFalse();
