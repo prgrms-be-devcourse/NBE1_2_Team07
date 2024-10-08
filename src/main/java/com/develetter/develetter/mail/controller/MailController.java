b963b3e094c62c9b5e19622cb7675ced93705054
ayoung-dev <dkdudab@hanmail.net>
2024-10-08 15:07:01 +0900

    Remove: MailController 사용하지 않아 삭제

--- src/main/java/com/develetter/develetter/mail/controller/MailController.java
+++
@@ -1,9 +0,0 @@
-package com.develetter.develetter.mail.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequiredArgsConstructor
-public class MailController {
-}
