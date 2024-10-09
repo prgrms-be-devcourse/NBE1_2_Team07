01690e3a5cb8d0c850cfaf953269dbd03c8f9323
최철진 <cil6879@gmail.com>
2024-10-09 19:59:36 +0900

    Chore: 코드 재병합

--- src/main/java/com/develetter/develetter/blog/repository/FilteredBlogRepository.java
+++ src/main/java/com/develetter/develetter/blog/repository/FilteredBlogRepository.java
@@ -2,6 +2,8 @@ package com.develetter.develetter.blog.repository;
 
 import com.develetter.develetter.blog.entity.FilteredBlog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +14,9 @@ public interface FilteredBlogRepository extends JpaRepository<FilteredBlog, Long
 
     // 특정 userId에 해당하는 가장 최근의 FilteredBlog를 반환
     Optional<FilteredBlog> findTopByUserIdOrderByCreatedAtDesc(Long userId);
+
+    // 3개월 이상된 레코드를 삭제하는 쿼리 메서드
+    @Modifying
+    @Query(value = "DELETE FROM filtered_blog WHERE created_at < NOW() - INTERVAL 3 MONTH", nativeQuery = true)
+    void deleteOldRecords();
 }
